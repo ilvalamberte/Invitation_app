4acb6a6ae998ea63916890b3e7450d87fea28b20
Suman Thapa Magar <suma0449@stud.kea.dk>
2021-04-10 14:11:13 +0200

    For windows setup

--- server/server.js
+++ server/server.js
@@ -6,13 +6,14 @@ const connectDB = require('../server/config/db')
 //connect to database
 connectDB()
 
+app.use(cors());
+app.options('*', cors());
 app.use(express.json({ extended: true }))
 
 app.use('/register', require('./routes/register'))
 app.use('test', require('./routes/test'))
 app.use('/auth', require('./routes/auth'))
 app.use('/guests', require('./routes/guests'))
-app.use(cors());
 
 
 const PORT = process.env.PORT || 5000
